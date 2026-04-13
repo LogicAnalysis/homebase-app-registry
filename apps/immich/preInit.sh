3595281bb665d09937512814703012eb4b4b814e
Ahmad Atiya <ahmad.m.atiya@gmail.com>
2026-04-13 17:41:53 -0400

    updated immich version, scripts

---
+++ apps/immich/preInit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+API_URL="http://localhost:2283/api"
+DATA_FILE="preInitData.json"
+
+# Parse JSON to extract the user parameters
+EMAIL=$(jq -r '.email' "$DATA_FILE")
+PASSWORD=$(jq -r '.password' "$DATA_FILE")
+NAME=$(jq -r '.name' "$DATA_FILE")
+
+echo "Waiting for Immich server to start..."
+until [ "$(curl -s -o /dev/null -w "%{http_code}" "$API_URL/server/ping")" -eq 200 ]; do
+    sleep 3
+done
+echo "Immich server is active."
+
+echo "Creating admin user..."
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" -X POST "$API_URL/auth/admin-sign-up" \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -d "{\"email\": \"$EMAIL\", \"password\": \"$PASSWORD\", \"name\": \"$NAME\"}")
+
+if [ "$STATUS_CODE" -eq 200 ] || [ "$STATUS_CODE" -eq 201 ]; then
+  echo "Admin user successfully created."
+  exit 0
+else
+  echo "Failed to create admin user. HTTP Status: $STATUS_CODE"
+  exit 1
+fi
--- apps/immich/preinstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "preinstall_script_version=0.0.1" >> bash_script_version.txt
